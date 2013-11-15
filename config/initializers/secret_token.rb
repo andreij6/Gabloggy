da2e7c6fce695d70aa3aeeb3a31d1ffa3110d7d1
Andre Jones <andreij6@gmail.com>
2013-11-14 23:27:36 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gablogger::Application.config.secret_token = 'a65ab33cf6d962e3f51d6399cd57561321e546f604a92dd3528dfd58604bf85ec3f7f59c380ace919422204c154592d02228260c788106e2effb916f8da05750'
