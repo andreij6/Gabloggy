36f2eb97ce4f9ed9dc18c2c41eaf983d5fb95ca6
Andre Jones <andreij6@gmail.com>
2013-11-19 15:20:16 -0600

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+recipes = Recipe.create(
+                      [{title: "California Chicken"},
+                        {name: "Meatballs" },
+                        {title: "Fish and Chips"}])   
