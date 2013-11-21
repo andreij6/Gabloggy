e958f514ea4a5bffa8327e3345d48dc513407b09
Andre Jones <andreij6@gmail.com>
2013-11-20 22:22:31 -0600

    css change homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @recipes = Recipe.all
+    @fits    = Fitness.all
+    @techs   = Tech.all
+    @events  = CurrentEvent.all
   end
 end
